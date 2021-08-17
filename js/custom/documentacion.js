b3e146b567f2749830a8a8d7450d48394a8d4161
Rony Stico <ronystico@gmail.com>
2021-08-17 00:21:26 -0400

    las anclas dan vueltas con pro switch y this

--- js/custom/documentacion.js
+++ js/custom/documentacion.js
@@ -1,8 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    var lol = document.getElementsByClassName('gray');
-    for(i = 0;i < lol.length;i++){
-        if(lol[i].ariaExpanded) {
-            lol[i].insertRule('transform: rotate(90deg);')
-        }
-    } 
-    });
+function Voltear(ivamn) {
+    switch (ivamn.id) {
+        case "a1":
+            document.getElementById("fa1").classList.toggle("transformancle");
+            break;
+        case "a2":
+            document.getElementById("fa2").classList.toggle("transformancle");
+            break;
+        case "a3":
+            document.getElementById("fa3").classList.toggle("transformancle");
+            break;
+        case "a4":
+            document.getElementById("fa4").classList.toggle("transformancle");
+            break;
+        case "a5":
+            document.getElementById("fa5").classList.toggle("transformancle");
+            break;
+        case "a6":
+            document.getElementById("fa6").classList.toggle("transformancle");
+            break;
+    }
+}
