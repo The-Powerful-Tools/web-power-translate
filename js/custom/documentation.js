570d98b7eae196ee490a01f507d6dd56a45d4650
Ivamn <user.name>
2021-08-19 17:40:57 -0300

    VAMO LO PIBE JS ANDATE A TU CASA

--- js/custom/documentation.js
+++ js/custom/documentation.js
@@ -1,32 +1,26 @@
 function Voltear(ivamn) {
     switch (ivamn.id) {
         case "a1":
-            document.getElementById("fa1").classList.toggle("transformancle");
             document.getElementById("a1").classList.toggle("gray");
             document.getElementById("a1").classList.toggle("black");
             break;
         case "a2":
-            document.getElementById("fa2").classList.toggle("transformancle");
             document.getElementById("a2").classList.toggle("gray");
             document.getElementById("a2").classList.toggle("black");
             break;
         case "a3":
-            document.getElementById("fa3").classList.toggle("transformancle");
             document.getElementById("a3").classList.toggle("gray");
             document.getElementById("a3").classList.toggle("black");
             break;
         case "a4":
-            document.getElementById("fa4").classList.toggle("transformancle");
             document.getElementById("a4").classList.toggle("gray");
             document.getElementById("a4").classList.toggle("black");
             break;
         case "a5":
-            document.getElementById("fa5").classList.toggle("transformancle");
             document.getElementById("a5").classList.toggle("gray");
             document.getElementById("a5").classList.toggle("black");
             break;
         case "a6":
-            document.getElementById("fa6").classList.toggle("transformancle");
             document.getElementById("a6").classList.toggle("gray");
             document.getElementById("a6").classList.toggle("black");
             break;
